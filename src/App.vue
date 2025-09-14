8804cdb544e94ba1abaddb99398126d7352c9511
ToxicStarfall <mr.dumbosayshi@gmail.com>
2025-09-14 08:15:29 -0500

    Reformatting and start page mockup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import MainHeader from './components/MainHeader.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+	<!-- <header>
+		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+		<div class="wrapper">
+			<HelloWorld msg="You did it!"/>
+		</div>
+	</header> -->
 
-  <main>
-    <TheWelcome />
-  </main>
+	<MainHeader />
+
+	<hr>
+
+	<main>
+		<!-- <TheWelcome/> -->
+		Request Authorization ->
+		Authorize Existing User ->
+
+		<hr>
+		Authorization Logs
+		- - -
+	</main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+	line-height: 1.5;
+	display: flex;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+	display: block;
+	width: 100px; height: 100px;
+	/* margin: 0 auto 2rem; */
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+hr {
+	width: 100%;
+    height: 2px;
+    border: none;
+    background-image: linear-gradient(90deg, transparent, 40%, #ffffff, 60%, transparent);
 }
+
+main {
+	padding: 2rem 4rem;
+}
+/* @media (min-width: 1024px) {
+	header {
+		display: flex;
+		place-items: center;
+		padding-right: calc(var(--section-gap) / 2);
+	}
+
+	.logo {
+		margin: 0 2rem 0 0;
+	}
+
+	header .wrapper {
+		display: flex;
+		place-items: flex-start;
+		flex-wrap: wrap;
+	}
+} */
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,49 @@
+<script setup>
+</script>
+
+<template>
+	<header>
+		<img alt="Vue logo" class="logo" src="../assets/logo.svg" width="80" height="80" />
+
+		<div class="wrapper">
+			<h2 class="green">Polar Operations Corporation<!-- Abandoned Polar Bear Breeding Facility --></h2>
+			<h4>Personnel Access Page</h4>
+		</div>
+
+		<button>Authorize</button>
+	</header>
+</template>
+
+<style scoped>
+	header {
+		line-height: 1.5;
+		padding: 1rem 2rem;
+		display: flex;
+	}
+	
+	.logo {
+		padding: 0 1rem;
+		display: block;
+		align-self: center;
+		/* margin: 0 auto 2rem; */
+	}
+	
+	.wrapper {
+		width: 100%;
+		display: flex;
+		justify-content: center; align-content: center;
+		flex-direction: column;
+    	flex-wrap: wrap;
+	}
+
+	h2, h4 {
+		width: auto;
+		font-size: 40px;
+		font-weight: 500;
+		text-align: center;
+	}
+	h4 {
+		font-size: 20px;
+		font-weight: 500;
+	}
+</style>
